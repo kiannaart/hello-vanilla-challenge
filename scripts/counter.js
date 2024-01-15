b9ed10bc8a8f18efbae3ba15c3323cbec7ce8a51
DKH <derek.hawryluk@edu.sait.ca>
2024-01-15 14:31:24 -0700

    added files

---
+++ scripts/counter.js
@@ -0,0 +1,16 @@
+function setupCounter(element) {
+  let counter = 0;
+  const setCounter = (count) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
+
+setupCounter(document.querySelector('#counter'));
+
+function darkMode () {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
